f9e0ce56676aa53a13abcc73780bf3e3de308a48
Edson Boldrini <edsonboldrini@gmail.com>
2022-11-13 23:11:04 -0300

    Update index js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ import Menu from '../src/components/Menu'
 import { StyledTimeline } from '../src/components/Timeline'
 
 function HomePage () {
-  const customStyle = {
-    // backgroundColor: 'red'
-  }
   return (
     <>
       <CSSReset />
-      <div style={customStyle}>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        // backgroundColor: "red",
+      }}>
         <Menu />
         <Header />
         <Timeline playlists={config.playlists} />
@@ -35,6 +37,7 @@ const StyledHeader = styled.div`
     border-radius: 50%;
   }
   .user-info {
+    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -68,18 +71,20 @@ function Timeline (props) {
         return (
           <section>
             <h2>{name}</h2>
-            {
-              videos.map((video) => {
-                return (
-                  <a href={video.url}>
-                    <img src={video.thumb} />
-                    <span>
-                      {video.title}
-                    </span>
-                  </a>
-                )
-              })
-            }
+            <div>
+              {
+                videos.map((video) => {
+                  return (
+                    <a href={video.url}>
+                      <img src={video.thumb} />
+                      <span>
+                        {video.title}
+                      </span>
+                    </a>
+                  )
+                })
+              }
+            </div>
           </section>
         )
       })}
